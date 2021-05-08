0085e299a5bbbfae990a35c17bc385a94b3d812b
sjbabadi <sheila.babadi@yahoo.com>
2021-05-08 17:05:11 -0400

    add query parameter parsing to /snippet route

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
   "log"
   "net/http"
+	"strconv"
 )
 
 // Define a home handler func which writes a byte slice as the res body
@@ -16,13 +18,18 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-  w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
   if r.Method != http.MethodPost {
 	w.Header().Set("Allow", http.MethodPost)
-	http.Error(w, "Method Not Allowed, 405)
+	http.Error(w, "Method Not Allowed", 405)
 	return
   }
   w.Write([]byte("Create a new snippet..."))
