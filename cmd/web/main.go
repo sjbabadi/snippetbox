c1882798018cfa520cfef4242bfd66f1caa27d58
sjbabadi <sheila.babadi@yahoo.com>
2021-05-09 09:35:47 -0400

    successfully connect to postgres

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,10 +2,13 @@
 package main
 
 import (
+		"database/sql"
     "log"
     "net/http"
 		"flag"
 		"os"
+		"fmt"
+		_ "github.com/lib/pq"
 )
 
 type application struct {
@@ -14,24 +17,49 @@ type application struct {
 }
 
 func main() {
-		addr := flag.String("addr", ":4000", "HTTP network address")
-		flag.Parse()
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-		infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-		errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app := &application{
+		errorLog: errorLog,
+		infoLog: infoLog,
+	}
 
-		app := &application{
-			errorLog: errorLog,
-			infoLog: infoLog,
-		}
+	const (
+		host = "localhost"
+		port = 5432
+		user = "sjbabadi"
+		dbname = "snippetbox_dev"
+	)
 
-		srv := &http.Server{
-			Addr: *addr,
-			ErrorLog: errorLog,
-			Handler: app.routes(),
-		}
+	connectionStr := fmt.Sprint("host=%s port=%s user=%s "+
+		"dbname=%s sslmode=disable",
+		host, port, user, dbname)
 
-    infoLog.Printf("Starting server on %s", *addr)
-    err := srv.ListenAndServe()
-    errorLog.Fatal(err)
+	db, err := sql.Open("postgres", connectionStr)
+	if err != nil {
+		app.errorLog.Println(err.Error())
+	}
+
+	err = db.Ping()
+	if err != nil {
+		app.errorLog.Println(err.Error())
+	}
+	
+	app.infoLog.Println("Database successfully connected!")
+	defer db.Close()
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+
+	srv := &http.Server{
+		Addr: *addr,
+		ErrorLog: errorLog,
+		Handler: app.routes(),
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module com.sjbabadi/snippetbox
 
 go 1.15
+
+require github.com/lib/pq v1.10.1
