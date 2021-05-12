b6155521a5bb723a78fd46997f834cf68e668f63
sjbabadi <sheila.babadi@yahoo.com>
2021-05-12 09:48:08 -0400

    add functionality to snippets.Get and handlers.showSnippet

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,8 @@ import (
 	"html/template"
 	"net/http"
 	"strconv"
+	"errors"
+	"com.sjbabadi/snippetbox/pkg/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -40,7 +42,17 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific snippet with ID %d", id)
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	fmt.Fprintf(w, "%v", snippet)
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
--- pkg/models/pg/snippets.go
+++ pkg/models/pg/snippets.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"database/sql"
 	"com.sjbabadi/snippetbox/pkg/models"
+	"errors"
 )
 
 type SnippetModel struct {
@@ -26,7 +27,21 @@ func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
 }
 
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
-	return nil, nil
+	stmt := `SELECT id, title, content, created_at, expires_at FROM snippets
+	WHERE expires_at > now() AND id = $1`
+
+	row := m.DB.QueryRow(stmt, id)
+
+	s := &models.Snippet{}
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.CreatedAt, &s.ExpiresAt)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, models.ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+	return s, nil
 }
 
 func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
