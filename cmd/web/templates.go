608470e9297cff491edabe27402a3d2e0ea3656f
sjbabadi <sheila.babadi@yahoo.com>
2021-06-12 11:47:21 -0400

    add templateData type to hold dynamic data for template rendering

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,6 +2,8 @@ package main
 
 import "com.sjbabadi/snippetbox/pkg/models"
 
+//this type struct will act as a holding structure for data passed to a template
 type templateData struct {
-	Snippet *models.Snippet
+	Snippet  *models.Snippet
+	Snippets []*models.Snippet
 }
